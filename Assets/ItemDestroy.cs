6e371944e99cd364a3230f2bfb53f0fd0d9a13a3
rn1y4 <himeka.miyamoto@accenture.com>
2024-01-23 18:09:38 +0900

    課題修正(画面外に出たアイテム削除)

--- Assets/ItemDestroy.cs
+++ Assets/ItemDestroy.cs
@@ -4,27 +4,27 @@ using UnityEngine;
 
 public class ItemDestroy : MonoBehaviour
 {
+    //MainCameraオブジェクトを取得
+    private GameObject mainCameraObj;
+    Camera cam;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //カメラオブジェクトの取得
+        mainCameraObj = GameObject.Find("Main Camera");
+        cam = mainCameraObj.GetComponent<Camera>();
     }
 
     // Update is called once per frame
     void Update()
     {
         //画面外のオブジェクトを破棄する
-      if (!GetComponent<Renderer>().isVisible)
+      if (gameObject.transform.position.z < mainCameraObj.transform.position.z)
       {
-            Debug.Log("DESTROY" + this.gameObject.name);
+            Debug.Log("DESTROY " + this.gameObject.name);
             Destroy(this.gameObject);
        }
     }
-
-    //private void OnBecameInvisible()
-    //{
-    //    Debug.Log("DESTROY" + this.gameObject.name);
-    //    Destroy(this.gameObject);
-   // }
 }
